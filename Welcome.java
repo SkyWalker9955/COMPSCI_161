e7413d7c9de1573413a839122ed0c1a8b6bdce7a
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-02-21 11:51:49 -0600

    Welcome

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+
+public class Welcome {
+	public static void main(String[] args) {
+		//Displaying 3 messages
+		
+		System.out.println("Welcome to Java" + "\nWelcome to Computer Science" + "\nProgramming is fun");
+	}
+}
