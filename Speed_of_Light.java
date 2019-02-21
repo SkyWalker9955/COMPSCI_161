431b48cc25d551f58d53fab62779bfee4243796e
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-02-21 14:03:18 -0600

    Update Speed_of_Light.java

--- Speed_of_Light.java
+++ Speed_of_Light.java
@@ -1,30 +1,47 @@
-import java.util.Scanner;
+import java.awt.List;
+import java.util.*;
 
-//Defines the time needed to reach the space body
-public class Speed_of_Light {
+public class Speed {
 
 	public static void main(String[] args) {
+		//Defines the time needed to reach the space body 
+		
 		
 		//declare and assign the variable speed_of_light per hour 
 		double speed_of_light;
 		speed_of_light = 300000;//km per second
 		
+		//Declare list of variable corresponding to planets 
+		/*
+		 * String Mars = Mars;
+		 * String Venus = Venus;
+		 * String Jupiter = Jupiter";
+		 */
+		int Mars = 227_900_000;
+		int Venus = 108_200_000;
+		int Jupiter = 778_500_000;
+		
+		//Declare a list 
+				new ArrayList<String>();
+				Arrays.asList(Mars, Venus, Jupiter);
 		
 		Scanner input = new Scanner(System.in);
 		//Prompt the user
 		System.out.println("Enter distance in kilometers to the space body ?: ");
-		double distance = input.nextDouble(); //Receives users input
+		int distance = input.nextInt();  //Receives users input
 		//Once finished 
-		input.close();
+		input.close(); 
 		
 		//Declare and compute the time in years 
 		double time;
 		time = distance / speed_of_light / 60;
+		double time_hours = time / 60;
 		
 		if (time > 60) {
-			System.out.println("The light needs " + time / 60  + " hours to get there!");
+			System.out.println("The light needs " + time_hours  + " hours to get there!");
 		} else {
-			System.out.println("The light needs " + time  + " minutes to get there!");
+			System.out.println("The light needs " + ((int)(time * 100.0) / 100.0)  + " minutes to get there!");
 		}
-	}	
+
+	}
 }
