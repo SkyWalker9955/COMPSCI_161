d2296498858dd82fcbb51783c0e0dc60d5d0c6db
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-02-26 13:25:06 -0600

    Create Lottery.java
    Unfinished

---
+++ Lottery.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Lottery {
+
+	public static void main(String[] args) {
+		
+		//Create Scanner 
+		Scanner input = new Scanner(System.in);
+		
+		//Generate a winning combination 
+		int winning_comb = (int)(Math.random() * 100);
+		
+		//System.out.println(winning_comb);
+		//Prompt 
+		System.out.println("Enter your lottery pick (two digits): ")
+		int guess = input.nextInt();
+		
+		
+		
+		
+	}
+
+}
