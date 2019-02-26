cea8e9998906f2c7947ce033ee20d96f4a47de2d
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-02-26 10:59:23 -0600

    Create IsLeapYear.java

---
+++ IsLeapYear.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class LeapYear {
+	
+	public static void main(String[] args) {
+	
+	//Create Scanner
+	Scanner input = new Scanner(System.in);
+	
+	//Prompt 
+	System.out.println("Enter the year: ");
+	int LeapYear = input.nextInt();
+	
+	//Leap year formula
+	if (LeapYear % 4 == 0 && LeapYear % 100 != 0 || LeapYear % 400 == 0) {
+		System.out.println("Is a leap year.");
+		} else {
+		System.out.println("Not a leap year.");
+		}
+	}
+	
+}
