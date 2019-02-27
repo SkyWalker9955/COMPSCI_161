1c3ae7413be9987cca7d3bc67b9649c5b1343f69
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-02-27 14:28:56 -0600

    Update Lottery.java

--- Lottery.java
+++ Lottery.java
@@ -14,7 +14,36 @@ public class Lottery {
 		//Prompt 
 		System.out.println("Enter your lottery pick (two digits): ");
 		int guess = input.nextInt();
-			
+
+		//Get digits from lottery 
+		int firstDigit = winning_comb / 10;
+		int secondDigit = winning_comb % 10; 
+		
+		//Get digits from user input 
+		int firstUserDigit = guess / 10;
+		int secondUserDigit = guess % 10; 
+		
+		System.out.println("\nLottery nums are " + winning_comb);
+		
+		//Check the guess 
+		if (winning_comb == guess) { 
+			System.out.println("You won $10,000 !");
+		}
+		else if (firstDigit == secondUserDigit 
+				 && secondDigit == firstUserDigit) {
+			System.out.println("You won $3,000 !");
+		}
+		else if (firstDigit == firstUserDigit 
+			  || firstDigit == secondUserDigit
+			  || secondDigit == firstUserDigit
+			  || secondDigit == secondUserDigit) {
+			System.out.println("You won $1,000 !");
+		}
+		else {
+			System.out.println("Sorry, no match...");
+		}
+		input.close();
+		
 	}
 
 }
