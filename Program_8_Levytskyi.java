7ca26ec26953acd71fdcd2d9b677d361297410f8
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-04-09 11:05:09 -0500

    Update Program_8_Levytskyi.java

--- Program_8_Levytskyi.java
+++ Program_8_Levytskyi.java
@@ -9,19 +9,21 @@ public class Program_8_Levytskyi {
 		System.out.println("The program sums numbers from 0 to 1000"
 				+ "\nEnter a number: ");
 		int inp_num = input.nextInt();
-		int counter = 0;
-		sumDigits(inp_num);
+		input.close();
+		
+		//result 
+		System.out.println("The sum of numbers is: " + sumDigits(inp_num));
 	}
 	
 	public static int sumDigits(long n) {
 		//sum var
 		int sum = 0;
 		//condition
-		for (n; n > 0; n++) { 
+		while (n > 0) { 
 			sum += n % 10; 
 			n = n / 10;
-		
-		return sum;
 		}
+		return sum;
 	}
+	
 }
